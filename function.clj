e44406ec842dbd45b8112e7ed876bd0cca6cd9ff
Peyman <Peyman@Peymans-iMac.local>
2022-12-05 09:48:30 +0330

    unify-diet-data

---
+++ function.clj
@@ -0,0 +1,8 @@
+(ns function)
+
+(def human-consumption   [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
