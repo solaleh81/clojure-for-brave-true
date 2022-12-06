0d4b51c7ed246b9b9c19902345df3a8e5190e9f4
Peyman <Peyman@Peymans-iMac.local>
2022-12-06 09:35:20 +0330

    take-while function

--- function.clj
+++ function.clj
@@ -65,3 +65,18 @@
 (take 3 [1 2 3 4 5 6 7 8 9])
 
 (drop 3 [1 2 3 4 5 6 7 8 9])
+
+(def food-journal
+  [
+   {:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}
+  ]
+  )
+
+(take-while #(< (:month %) 3) food-journal)
