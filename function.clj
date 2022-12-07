ab9324a1fbf3b1d23e0df09ee084c75f27c18535
Peyman <Peyman@Peymans-iMac.local>
2022-12-07 10:10:11 +0330

    vampire function

--- function.clj
+++ function.clj
@@ -104,3 +104,28 @@
 (sort [3 1 2])
 
 (sort-by count ["aaa" "c" "bb"])
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true :name "McMackson"}
+   2 {:makes-blood-puns? true, :has-pulse? false :name "Damon Salvator"}
+   3 {:makes-blood-puns? true}, :has-pulse? true :name "Mickey Mouse"})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn indentify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+
+(time (vampire-related-details 0))
