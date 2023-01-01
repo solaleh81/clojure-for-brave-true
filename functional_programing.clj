d7a500b010b476a057d6762f5cd77203c2220851
Peyman <Peyman@Peymans-iMac.local>
2023-01-01 08:59:41 +0330

    not referentially

--- functional_programing.clj
+++ functional_programing.clj
@@ -5,3 +5,19 @@
   (str words ", Daniel-san"))
 
 (wisdom "Always bathe on Fridays")
+
+(defn year-end-evaluation
+  []
+  (if (> (rand) 0.5)
+    "You get a raise"
+    "Better luck next year"))
+
+
+(defn analyze-file
+  [filename]
+  (analysis (slurp filename)))
+
+
+(defn analysis
+  [text]
+  (str "Character count: " (count text)))
