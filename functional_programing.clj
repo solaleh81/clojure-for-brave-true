deeaca0e3fe27a9ac74f05d04409b202ae0c7464
Peyman <Peyman@Peymans-iMac.local>
2023-01-01 09:24:00 +0330

    Clojure lets you work around this apparent limitation with recursion

--- functional_programing.clj
+++ functional_programing.clj
@@ -21,3 +21,23 @@
 (defn analysis
   [text]
   (str "Character count: " (count text)))
+
+(def great-baby-name "Rosanthony")
+
+(let [great-baby-name "Bloodthunder"]
+  great-baby-name)
+
+great-baby-name
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     (sum (rest vals) (+ (first vals) accumulating-total)))))
+
+(sum [39 5 1]) ; single-arity body calls two-arity body
+(sum [39 5 1] 0)
+(sum [5 1] 39)
+(sum [1] 44)
+(sum [] 45) ; base case is reached, so return accumulating-total
